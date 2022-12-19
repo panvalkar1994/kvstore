d80b816822ca350aba27f6a812ebecf47fe6984b
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2022-12-20 00:23:37 +0530

    basic get set in database done

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+pub struct Database {
+    map: HashMap<String, String>,
+}
+
+#[derive(Debug)]
+pub struct Query {
+    operation: Operation,
+    args: OpArgs,
+}
+
+#[derive(Debug)]
+enum OpArgs {
+    Pair((String, String)),
+    Single(String),
+    None,
+}
+
+#[derive(Debug)]
+enum Operation {
+    Get,
+    Set,
+    Exit,
+}
+
+impl Database {
+    pub fn new() -> Self {
+        Database {
+            map: HashMap::new(),
+        }
+    }
+    pub fn set(&mut self, key: String, val: String) -> String {
+        match self.map.insert(key, val) {
+            Some(v) => v.to_string(),
+            None => format!("New Key Added"),
+        }
+    }
+
+    pub fn get(&mut self, key: String) -> String {
+        match self.map.get(&key) {
+            Some(v) => v.to_string(),
+            None => format!("No Value found for {}", key),
+        }
+    }
+
+    pub fn parse(&self, line: &String) -> Result<Query, String> {
+        let line = line.trim().to_string();
+        let tokens: Vec<&str> = line.split(" ").collect();
+        if tokens[0] != "store" {
+            return Err(format!("query should start with `store`"));
+        }
+        let mut args = OpArgs::None;
+        let operation = match tokens[1] {
+            "exit" => Operation::Exit,
+            "get" => {
+                args = OpArgs::Single(tokens[2].to_string());
+                Operation::Get
+            }
+            "set" => {
+                args = OpArgs::Pair((tokens[2].to_string(), tokens[3].to_string()));
+                Operation::Set
+            }
+            _ => return Err(format!("Wrong operation")),
+        };
+
+        Ok(Query { operation, args })
+    }
+
+    pub fn exec(&mut self, query: &Query) -> Option<String> {
+        match &query.args {
+            OpArgs::Pair((k, v)) => Some(self.set(k.to_string(), v.to_string())),
+            OpArgs::Single(k) => Some(self.get(k.to_string())),
+            OpArgs::None => std::process::exit(0),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use kvstore::Database;
+use std::io;
+
+fn main() -> ! {
+    println!("---------------Database Name: KVstore------------");
+    let mut db = Database::new();
+    loop {
+        let mut input=String::new();
+        io::stdin().read_line(&mut input).expect("Bad Input");
+        let mut query = db.parse(&input).unwrap();
+        if let Some(v) = db.exec(&mut query) {
+            println!("Result>{}",v);
+        } else {
+            println!("------------");
+        }
+    }
 }
